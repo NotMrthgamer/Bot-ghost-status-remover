7c5d0f32eeddc87b31a00c0cb7cb7e49641d08d6
Mr.T.H GameR <108679146+NotMrthgamer@users.noreply.github.com>
2024-12-07 11:03:55 +0600

    index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["🎮 Playing WorldsHub Server"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
